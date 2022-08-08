ba9ec92435c09953c490cb90960780510e2b41a7
Dan Cross <dancross96@gmail.com>
2022-08-07 20:49:36 -0500

    added the show action

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -3,4 +3,10 @@ class PetsController < ApplicationController
     pets = Pet.all
     render json: pets.as_json
   end
+
+  def show
+    pet = Pet.find_by(id: params[:id])
+    render json: pet.as_json
+  end
+
 end
